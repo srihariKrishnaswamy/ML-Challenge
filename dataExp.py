2c10587024db950db2e7b6f91418349ef6db841a
Srihari Krishnaswamy <srihari@uw.edu>
2023-05-04 13:06:57 -0700

    basic writing working

--- dataExp.py
+++ dataExp.py
@@ -3,22 +3,20 @@ import os
 # Path to the output file
 output_file = 'output.txt'
 # Path to the directory containing the output files
-output_path = 'runs/detect/exp'
+output_path = 'runs/detect/exp/labels/'
 
 # Open the output file for writing
 with open(output_file, 'w') as f_out:
-    f_out.write('ACCESSING OUTPUT FILE')
+    f_out.write('ACCESSING OUTPUT FILE \n')
     # Loop through all the output files
-    for txt_file in glob.glob(os.path.join(output_path, '*.txt')):
+    for txt_file in glob.glob(output_path + '*.txt'):
         # Read the contents of the file
-        with open(txt_file, 'r') as f_in:
-            line = f_in.readlines()
-
-            class_id, *bbox = line.strip().split()
-
-            # Convert the class ID and bounding box coordinates to the desired format
-            class_id = int(class_id)
-            bbox = list(map(float, bbox))
-
-            # Write the class and bounding box information to the output file
-            f_out.write(f'Class ID: {class_id}, Bounding box: {bbox}\n')
+      with open(txt_file, 'r') as f_in:
+        lines = f_in.readlines()
+        # class_id, *bbox = line.strip().split()
+        # # Convert the class ID and bounding box coordinates to the desired format
+        # class_id = int(class_id)
+        # bbox = list(map(float, bbox))
+        # # Write the class and bounding box information to the output file
+        # f_out.write(f'Class ID: {class_id}, Bounding box: {bbox}\n')
+        f_out.write(lines[0])
