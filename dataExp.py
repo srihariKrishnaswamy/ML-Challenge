ab89082b7c04dbca7a9329c7da80a62c44e49c44
Srihari Krishnaswamy <srihari@uw.edu>
2023-05-04 12:35:47 -0700

    please work

--- dataExp.py
+++ dataExp.py
@@ -1,53 +1,30 @@
-# import glob
-# import os
-
-# # Path to the directory containing the output files
-# output_path = 'runs/detect/exp'
-
-# # Loop through all the output files
-# for txt_file in glob.glob(os.path.join(output_path, '*.txt')):
-#     # Read the contents of the file
-#     with open(txt_file, 'r') as f:
-#         lines = f.readlines()
-#     print('IN FIRST FOR LOOP')
-#     # Extract the class and bounding box information for each object
-#     for line in lines:
-#         # Split the line into class ID and bounding box coordinates
-#         class_id, *bbox = line.strip().split()
-        
-#         # Convert the class ID and bounding box coordinates to the desired format
-#         class_id = int(class_id)
-#         bbox = list(map(float, bbox))
-        
-#         # Print the class and bounding box information
-#         print(f'Class ID: {class_id}, Bounding box: {bbox}')
 import glob
 import os
-
-# Path to the directory containing the output files
-output_path = 'runs/detect/exp'
 
 # Path to the output file
 output_file = 'output.txt'
 with open(output_file, 'w') as f:
     f.write('ACCESSING OUTPUT FILE')
-# Open the output file for writing
-with open(output_file, 'w') as f_out:
+# Path to the directory containing the output files
+output_path = 'runs/detect/exp'
 
-    # Loop through all the output files
-    for txt_file in glob.glob(os.path.join(output_path, '*.txt')):
-        # Read the contents of the file
-        with open(txt_file, 'r') as f_in:
-            lines = f_in.readlines()
+# # Open the output file for writing
+# with open(output_file, 'w') as f_out:
 
-        # Extract the class and bounding box information for each object
-        for line in lines:
-            # Split the line into class ID and bounding box coordinates
-            class_id, *bbox = line.strip().split()
+#     # Loop through all the output files
+#     for txt_file in glob.glob(os.path.join(output_path, '*.txt')):
+#         # Read the contents of the file
+#         with open(txt_file, 'r') as f_in:
+#             lines = f_in.readlines()
 
-            # Convert the class ID and bounding box coordinates to the desired format
-            class_id = int(class_id)
-            bbox = list(map(float, bbox))
+#         # Extract the class and bounding box information for each object
+#         for line in lines:
+#             # Split the line into class ID and bounding box coordinates
+#             class_id, *bbox = line.strip().split()
 
-            # Write the class and bounding box information to the output file
-            f_out.write(f'Class ID: {class_id}, Bounding box: {bbox}\n')
+#             # Convert the class ID and bounding box coordinates to the desired format
+#             class_id = int(class_id)
+#             bbox = list(map(float, bbox))
+
+#             # Write the class and bounding box information to the output file
+#             f_out.write(f'Class ID: {class_id}, Bounding box: {bbox}\n')
