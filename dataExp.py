aa98dd96a7778bd011fde6296f006e3e24461488
Srihari Krishnaswamy <srihari@uw.edu>
2023-05-05 10:48:02 -0700

    multiple videos to one excel spreadsheet hopefully supported

--- dataExp.py
+++ dataExp.py
@@ -37,6 +37,7 @@ os.remove("sourceVid.txt")
 #getting data in list of dictionaries to be sorted
 data = []
 excelName = "detections.xlsx"
+worksheetName = "detections"
 with open(second_output_file, 'r') as sf_in:
     lines = sf_in.readlines()
     for line in lines:
@@ -52,21 +53,34 @@ with open(second_output_file, 'r') as sf_in:
         data.append(dict)
 sortedData = sorted(data, key=lambda k: int(k['frame'])) #sorting data by frame 
 # write to excel from list of dictionaries
-workbook = xlsxwriter.Workbook(excelName)
-worksheet = workbook.add_worksheet("detections")
-worksheet.write(0,0,"Source Video")
-worksheet.write(0,1,"Current Frame")
-worksheet.write(0,2,"Classification")
-worksheet.write(0,3,"X Bound, Left")
-worksheet.write(0,4,"X Bound, Right")
-worksheet.write(0,5,"Y Bound, Upper")
-worksheet.write(0,6,"Y Bound, Lower")
-for index, entry in enumerate(sortedData):
-    worksheet.write(index+1, 0, entry["vid"])
-    worksheet.write(index+1, 1, entry["frame"])
-    worksheet.write(index+1, 2, entry["class"])
-    worksheet.write(index+1, 3, entry["x_left"])
-    worksheet.write(index+1, 4, entry["x_right"])
-    worksheet.write(index+1, 5, entry["y_up"])
-    worksheet.write(index+1, 6, entry["y_low"])
+if os.path.exists(excelName):
+    workbook = xlsxwriter.Worksbook(excelName)
+    worksheet = workbook.get_worksheet_by_name(worksheetName)
+    last_row = worksheet.dim_rowmax
+    for row, entry in enumerate(sortedData, start=last_row+1):
+        worksheet.write(row, 0, entry["vid"])
+        worksheet.write(row, 1, entry["frame"])
+        worksheet.write(row, 2, entry["class"])
+        worksheet.write(row, 3, entry["x_left"])
+        worksheet.write(row, 4, entry["x_right"])
+        worksheet.write(row, 5, entry["y_up"])
+        worksheet.write(row, 6, entry["y_low"])
+else:
+    workbook = xlsxwriter.Workbook(excelName)
+    worksheet = workbook.add_worksheet(worksheetName)
+    worksheet.write(0,0,"Source Video")
+    worksheet.write(0,1,"Current Frame")
+    worksheet.write(0,2,"Classification")
+    worksheet.write(0,3,"X Bound, Left")
+    worksheet.write(0,4,"X Bound, Right")
+    worksheet.write(0,5,"Y Bound, Upper")
+    worksheet.write(0,6,"Y Bound, Lower")
+    for index, entry in enumerate(sortedData):
+        worksheet.write(index+1, 0, entry["vid"])
+        worksheet.write(index+1, 1, entry["frame"])
+        worksheet.write(index+1, 2, entry["class"])
+        worksheet.write(index+1, 3, entry["x_left"])
+        worksheet.write(index+1, 4, entry["x_right"])
+        worksheet.write(index+1, 5, entry["y_up"])
+        worksheet.write(index+1, 6, entry["y_low"])
 workbook.close()
