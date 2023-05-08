06bc22845974de6c76720cf119f0d4263b703040
Srihari Krishnaswamy <srihari@uw.edu>
2023-05-07 19:01:51 -0700

    additions to script

--- master_detect_data.py
+++ master_detect_data.py
@@ -9,40 +9,39 @@ def_output_folder = "out"
 full_output_path = ""
 yolo_output_path_log = "output_path_log.txt"
 def determine_output_folder(): #assumes that the base output folder exists, creates new output folder
-    output_list = os.listdir(output_folder)
-    max = 0
-    for folder in output_list:
-        if len(folder) == len(def_output_folder):
-            max = 2
-        else:
-            folder_num = int(folder[3:len(folder)])
-            if folder_num >= max:
-                max = folder_num + 1
-    if max == 0:
-        new_output_folder = os.path.join(output_folder,def_output_folder + "/")
-    else:
-        new_output_folder = os.path.join(output_folder,def_output_folder + str(max) + "/")
-    return new_output_folder
+  output_list = os.listdir(output_folder)
+  max = 0
+  for folder in output_list:
+      if len(folder) == len(def_output_folder):
+          max = 2
+      else:
+          folder_num = int(folder[3:len(folder)])
+          if folder_num >= max:
+              max = folder_num + 1
+  if max == 0:
+      new_output_folder = os.path.join(output_folder,def_output_folder + "/")
+  else:
+      new_output_folder = os.path.join(output_folder,def_output_folder + str(max) + "/")
+  return new_output_folder
 while choice != "quit":
-    print("Enter video to process, enter quit to quit (must be valid video in folder)")
-    choice = input()
-    if choice == "quit":
-        break
-    elif os.path.exists(os.path.join("./videos", choice)) == False:
-        print("path does not exist")
-    else:
-        vids.append(os.path.join("./videos", choice))
+  print("Enter video to process, enter quit to quit (must be valid video in folder)")
+  choice = input()
+  if choice == "quit":
+      break
+  elif os.path.exists(os.path.join("./videos", choice)) == False:
+      print("path does not exist")
+  else:
+      vids.append(os.path.join("./videos", choice))
 print(vids)
-
 for video in vids:
-    print("curr vid: " + video)
-    proc1 = subprocess.Popen(['python', 'detect.py', '--weights', model, '--source', video, '--save-txt'])
-    proc1.communicate()
-    proc2 = subprocess.Popen(['python', 'dataExp.py'])
-    proc2.communicate()
+  print("curr vid: " + video)
+  proc1 = subprocess.Popen(['python', 'detect.py', '--weights', model, '--source', video, '--save-txt'])
+  proc1.communicate()
+  proc2 = subprocess.Popen(['python', 'dataExp.py'])
+  proc2.communicate()
 #video processing done and videos & excel file generated
 if os.path.exists(output_folder) == False:
-    os.mkdir(output_folder)
+  os.mkdir(output_folder)
 full_output_path = determine_output_folder()
 os.mkdir(full_output_path)
 print("OUTPUT FOLDER: " + full_output_path)
@@ -50,9 +49,13 @@ print("OUTPUT FOLDER: " + full_output_path)
 shutil.move("detections.xlsx", full_output_path)
 folders = []
 with open(yolo_output_path_log, 'r') as yolo_output:
-    for line in yolo_output.readlines():
-        folders.append(line.strip())
+  for line in yolo_output.readlines():
+      folders.append(line.strip())
+os.remove(yolo_output_path_log)
+print("FOLDERS: ")
+print(folders)
 for i in range(len(folders)):
-    vid = os.path.join(folders[i], os.listdir(folders[i])[0])
-    shutil.move(vid, full_output_path)
-    # os.remove(folder)
+  print("i: " + str(i))
+  vid = os.path.join(folders[i], os.listdir(folders[i])[0])
+  shutil.move(vid, full_output_path)
+  os.rmdir(folders[i])
