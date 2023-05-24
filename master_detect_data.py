9da1bc1153cbee6e132773137fe762b39ff3b590
Srihari Krishnaswamy <srihari@uw.edu>
2023-05-24 13:46:17 -0700

    safety measures added

--- master_detect_data.py
+++ master_detect_data.py
@@ -30,37 +30,36 @@ def determine_output_folder(): #assumes that the base output folder exists, retu
   else:
       new_output_folder = os.path.join(output_folder,def_output_folder + str(max) + "/")
   return new_output_folder
-
-args = parser.parse_args()
-vids = args.videos
-model = os.path.join("./iterations/", args.model)
-
-if len(vids) > 0:
-  for video in vids:
-    print("Current Video: " + video)
-    proc1 = subprocess.Popen(['python', 'detect.py', '--weights', model, '--source', os.path.join("videos/", video), '--save-txt'])
-    proc1.communicate()
-    proc2 = subprocess.Popen(['python', 'dataExp.py'])
-    proc2.communicate()
-  #video processing done and videos & excel file generated
-  if os.path.exists(output_folder) == False:
-    os.mkdir(output_folder)
-  full_output_path = determine_output_folder()
-  os.mkdir(full_output_path)
-  print("OUTPUT FOLDER: " + full_output_path)
-  # moving appropriate files
-  shutil.move("detections.xlsx", full_output_path)
-  folders = []
-  with open(yolo_output_path_log, 'r') as yolo_output:
-    for line in yolo_output.readlines():
-        folders.append(line.strip())
-  os.remove(yolo_output_path_log)
-  print("FOLDERS: ")
-  print(folders)
-  for i in range(len(folders)):
-    vid = os.path.join(folders[i], os.listdir(folders[i])[0])
-    shutil.move(vid, full_output_path)
-    os.rmdir(folders[i])
-    
-if os.path.exists("runs"):
-  shutil.rmtree("runs")
+if __name__ == "__main__":
+  args = parser.parse_args()
+  vids = args.videos
+  model = os.path.join("./iterations/", args.model)
+  if len(vids) > 0:
+    for video in vids:
+      print("Current Video: " + video)
+      proc1 = subprocess.Popen(['python', 'detect.py', '--weights', model, '--source', os.path.join("videos/", video), '--save-txt'])
+      proc1.communicate()
+      proc2 = subprocess.Popen(['python', 'dataExp.py'])
+      proc2.communicate()
+    #video processing done and videos & excel file generated
+    if os.path.exists(output_folder) == False:
+      os.mkdir(output_folder)
+    full_output_path = determine_output_folder()
+    os.mkdir(full_output_path)
+    print("OUTPUT FOLDER: " + full_output_path)
+    # moving appropriate files
+    shutil.move("detections.xlsx", full_output_path)
+    folders = []
+    with open(yolo_output_path_log, 'r') as yolo_output:
+      for line in yolo_output.readlines():
+          folders.append(line.strip())
+    os.remove(yolo_output_path_log)
+    print("FOLDERS: ")
+    print(folders)
+    for i in range(len(folders)):
+      vid = os.path.join(folders[i], os.listdir(folders[i])[0])
+      shutil.move(vid, full_output_path)
+      os.rmdir(folders[i])
+      
+  if os.path.exists("runs"):
+    shutil.rmtree("runs")
